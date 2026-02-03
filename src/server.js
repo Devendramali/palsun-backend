f302f37d1f7762728ed0ebd007211282e646a323
Devendramali <devendramali01234@gmail.com>
2026-02-03 23:25:57 +0530

    new addmin

--- src/server.js
+++ src/server.js
@@ -28,7 +28,19 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5174",
+    "http://localhost:5173",
+    "https://admin-palsun.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
+
+
 app.use(express.json());
 
 // Serve uploads folders statically **before API routes**
